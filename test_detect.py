ea9fb59529dc6bf4eef3fe0c91223f8de3005e5c
hungrhea <yuntingang140201@gmail.com>
2022-10-05 17:05:04 +0800

    .

---
+++ test_detect.py
@@ -0,0 +1,23 @@
+import detection
+import torch
+import subprocess
+
+print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
+p = subprocess.getstatusoutput("python detect.py --weights best_6223.pt --img 640 --conf 0.75 --source ./imagezmq_images/image2.jpg") 
+output = p[1]
+with open('outputs/output_dummy.txt', 'w') as f:    # path to output .txt file
+    f.write(output)
+message_dict = detection.process_output(path = "outputs/output_dummy.txt")
+
+print(message_dict)
+
+if detection.highest_conf(message_dict):
+    message=detection.highest_conf(message_dict)[1]
+else:
+    message=100
+
+
+
+message = str(message)
+print("message = ", message)
+
